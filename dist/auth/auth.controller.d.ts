96bf8a49373ac45ce3d1a7dc1c0dcba7d2e569f7
abhishek kumar <abhisir2803@gmail.com>
2025-10-22 18:31:45 +0530

    user creation

--- dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-import { LoginUserDto } from '../users/dto/login-user.dto';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    login(loginUserdto: LoginUserDto): Promise<{
-        access_token: string;
-    }>;
-}
