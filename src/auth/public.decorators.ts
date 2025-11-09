ec9f2ecf6f587125bdc34bf6209b8c9ebc94cb3d
abhishek kumar <abhisir2803@gmail.com>
2025-11-10 00:30:54 +0530

    clean main branch

--- src/auth/public.decorators.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-
-export const Is_Public_Key = 'isPublic'
-
-export const Public = ()=> SetMetadata(Is_Public_Key,true);
--- src/invoices/dto/create-invoice.dto.ts
+++
@@ -1,9 +0,0 @@
-import {IsString,IsNumber,IsDate} from 'class-validator'
-export class CreateInvoiceDto {
-    @IsString()
-    client_id:string
-    @IsNumber()
-    amount:number
-    @IsDate()
-    due_date:Date
-}
